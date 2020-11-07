e6bc23ae5d368f2d79cbc7917daa01b859841280
afwe <936017948@qq.com>
2020-11-07 20:32:36 +0800

    go

---
+++ wj/src/components/library/LibraryIndex.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-aside style="width: 200px;margin-top: 20px">
+      <switch></switch>
+      <SideMenu></SideMenu>
+    </el-aside>
+    <el-main>
+      <book></book>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  import SideMenu from './SideMenu'
+  const book=()=> import('./Books')
+  export default {
+    name: 'AppLibrary',
+    components:{SideMenu,book}
+  }
+</script>
+<style scoped>
+</style>
+
